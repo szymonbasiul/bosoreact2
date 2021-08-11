6ff8010279c192e248b0f3e06ad0069ddc0be0e5
szymo <szymon.basiul@wp.pl>
2021-08-11 23:27:25 +0200

    Skill star rating implementation. Code refactoring.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import { useRef } from 'react';
 
-export interface testProps{
+export interface testProps {
   event: React.MouseEvent<HTMLButtonElement>;
 }
 
@@ -12,20 +12,19 @@ export interface testProps{
 function App() {
 
   console.log("Zlo!")
-  const [backgroundColorState, setBackgroundColorState] = useState("white");
+  // const [backgroundColorState, setBackgroundColorState] = useState("white");
 
   const mainRef = useRef() as React.MutableRefObject<HTMLDivElement>
   const test = () => {
     console.log(mainRef.current.style.backgroundColor);
-    
   }
 
   const colorChanger = () => {
     mainRef.current.style.backgroundColor === "white" ? mainRef.current.style.backgroundColor = "black" : mainRef.current.style.backgroundColor = "white";
-    
   }
 
-  
+
+
   // const intro = "BoSo Company"
   const designer1 = "Bogdan T."
   const designer2 = "Szymon B."
@@ -35,43 +34,63 @@ function App() {
 
 
   const default_text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, minima ullam? Tempora minima, dolorem eligendi maiores expedita rem quod dignissimos natus! Nisi sed, molestiae voluptatum in minima accusamus autem fugit."
-  const user_face = require("./img/default_user.jpg").default;
-  const user_face2 = require("./img/default_user.jpg").default;
-  const logo = require("./img/bosoreactprojectlogo.png").default;
 
+  const imgPicker = (imgName: string) => {
+    return require("./img/" + imgName + ".png").default;
+  }
+
+  const starVariable = (<><img src={imgPicker("star_skill")} alt="star" />
+    <img src={imgPicker("star_skill")} alt="star" />
+    <img src={imgPicker("star_skill")} alt="star" />
+    <img src={imgPicker("star_skill")} alt="star" />
+    <img src={imgPicker("star_skill")} alt="star" /></>)
 
   return (
 
-
     <main ref={mainRef}>
-      <button className="color-mode" onClick={()=>{colorChanger()}}>Color Mode</button>
-      <img className="logotype" src={logo} alt="logo">
+      <button className="color-mode" onClick={() => { colorChanger() }}>Color Mode</button>
+      <div className="logoContainer">
+        <img className="logotype" src={imgPicker("default_user")} alt="logo">
         </img>
+      </div>
       <div className="personal-container">
         <div className="person-one-container">
           <div className="person-one">
             <div className="photo">
-              <img className="person-img" src={user_face} alt="user_face" />
+              <img className="person-img" src={imgPicker("default_user")} alt="user_face" />
             </div>
             <div className="personal-info">
-              {default_text}
+              {/* {default_text} */}
             </div>
           </div>
           <div className="personal-bar-one">
             {designer1}
           </div>
         </div>
-        
+
         <div className="person-two-container">
           <div className="personal-bar-two">
             {designer2}
           </div>
           <div className="person-two">
             <div className="photo">
-              <img className="person-img" src={user_face2} alt="user_face2" />
+              <img className="person-img" src={imgPicker("default_user")} alt="user_face2" />
             </div>
             <div className="personal-info">
-              {default_text}
+              {/* {default_text} */}
+              <ul>
+                <ol>
+                  SCSS/CSS {starVariable}
+                </ol>
+                <ol> Canvas {starVariable}
+                </ol>
+                <ol> React/Typescript {starVariable}
+                </ol>
+                <ol> Node/Express {starVariable}
+                </ol>
+                <ol> MySQL {starVariable}
+                </ol>
+              </ul>
             </div>
           </div>
         </div>
--- src/dist/App.js
+++ src/dist/App.js
@@ -1,29 +1,44 @@
 "use strict";
 exports.__esModule = true;
+var React = require("react");
 var react_1 = require("react");
 require("./App.css");
+var react_2 = require("react");
 function App() {
+    console.log("Zlo!");
+    var _a = react_1.useState("white"), backgroundColorState = _a[0], setBackgroundColorState = _a[1];
+    var mainRef = react_2.useRef();
+    var test = function () {
+        console.log(mainRef.current.style.backgroundColor);
+    };
+    var colorChanger = function () {
+        mainRef.current.style.backgroundColor === "white" ? mainRef.current.style.backgroundColor = "black" : mainRef.current.style.backgroundColor = "white";
+    };
     // const intro = "BoSo Company"
     var designer1 = "Bogdan T.";
     var designer2 = "Szymon B.";
+    var backgroundColor1 = "black";
+    var backgroundColor2 = "white";
     var default_text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, minima ullam? Tempora minima, dolorem eligendi maiores expedita rem quod dignissimos natus! Nisi sed, molestiae voluptatum in minima accusamus autem fugit.";
     var user_face = require("./img/default_user.jpg")["default"];
     var user_face2 = require("./img/default_user.jpg")["default"];
     var logo = require("./img/bosoreactprojectlogo.png")["default"];
-    return (react_1["default"].createElement("main", null,
-        react_1["default"].createElement("div", { className: "personal-container" },
-            react_1["default"].createElement("div", { className: "people" },
-                react_1["default"].createElement("div", { className: "person-one" },
-                    react_1["default"].createElement("div", { className: "photo" },
-                        react_1["default"].createElement("img", { className: "person-img", src: user_face, alt: "user_face" })),
-                    react_1["default"].createElement("div", { className: "personal-info" }, default_text)),
-                react_1["default"].createElement("div", { className: "personal-bar-one" }, designer1)),
-            react_1["default"].createElement("div", { className: "people" },
-                react_1["default"].createElement("div", { className: "personal-bar-two" }, designer2),
-                react_1["default"].createElement("div", { className: "person-two" },
-                    react_1["default"].createElement("div", { className: "photo" },
-                        react_1["default"].createElement("img", { className: "person-img", src: user_face2, alt: "user_face2" })),
-                    react_1["default"].createElement("div", { className: "personal-info" }, default_text)))),
-        react_1["default"].createElement("div", { className: "footer" }, "logo/contact us/ yt-fb-tweeter")));
+    return (React.createElement("main", { ref: mainRef },
+        React.createElement("button", { className: "color-mode", onClick: function () { colorChanger(); } }, "Color Mode"),
+        React.createElement("img", { className: "logotype", src: logo, alt: "logo" }),
+        React.createElement("div", { className: "personal-container" },
+            React.createElement("div", { className: "person-one-container" },
+                React.createElement("div", { className: "person-one" },
+                    React.createElement("div", { className: "photo" },
+                        React.createElement("img", { className: "person-img", src: user_face, alt: "user_face" })),
+                    React.createElement("div", { className: "personal-info" }, default_text)),
+                React.createElement("div", { className: "personal-bar-one" }, designer1)),
+            React.createElement("div", { className: "person-two-container" },
+                React.createElement("div", { className: "personal-bar-two" }, designer2),
+                React.createElement("div", { className: "person-two" },
+                    React.createElement("div", { className: "photo" },
+                        React.createElement("img", { className: "person-img", src: user_face2, alt: "user_face2" })),
+                    React.createElement("div", { className: "personal-info" }, default_text)))),
+        React.createElement("div", { className: "footer" }, "logo/contact us/ yt-fb-tweeter")));
 }
 exports["default"] = App;
