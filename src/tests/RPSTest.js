9e41e4f4489609b4a89f16bf9f5b4f12521256cc
szymo <szymon.basiul@wp.pl>
2022-01-03 21:31:07 +0100

    Setting up first RPSTests +change color on hover +choosen one with red border

--- src/tests/RPSTest.js
+++ src/tests/RPSTest.js
@@ -1,1 +1,40 @@
+import { Selector } from "testcafe";
+const gameStarter = Selector(".gameStarterButton");
+const rpsActionButton = Selector(".rpsActionButton");
+const rpsCpuActionButton = Selector(".cpuAction");
 fixture`RPS`.page`http://localhost:3000/rps`;
+
+// border gets red on hoover
+
+test("Make Border Red On Hover", async (t) => {
+	await t.click(gameStarter).hover(rpsActionButton);
+	await t
+		.expect(
+			rpsActionButton.getStyleProperty(
+				"border-top-color",
+				"border-bottom-color",
+				"border-left-color",
+				"border-right-color"
+			)
+		)
+		.eql("rgb(255, 0, 0)");
+});
+
+// RPS on click will stay as a chosen one
+
+test("Check if clicked RPS button stays with red border", async (t) => {
+	await t.click(gameStarter).click(rpsActionButton);
+	await t.hover(rpsCpuActionButton);
+	await t
+		.expect(
+			rpsActionButton.getStyleProperty(
+				"border-top-color",
+				"border-bottom-color",
+				"border-left-color",
+				"border-right-color"
+			)
+		)
+		.eql("rgb(255, 0, 0)");
+});
+
+// after player picks RPS computer get its choice visible on separate box
