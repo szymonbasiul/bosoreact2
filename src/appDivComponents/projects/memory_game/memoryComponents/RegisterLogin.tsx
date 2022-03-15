6124cf233b6f4de4a9f9fbb8a3491d28bcc2a015
szymo <szymon.basiul@wp.pl>
2022-03-15 22:48:34 +0100

    Created log in/register form, prepared database: tables and react component.
    Co-authored-by: vileider <vileider@users.noreply.github.com>

--- src/appDivComponents/projects/memory_game/memoryComponents/RegisterLogin.tsx
+++ src/appDivComponents/projects/memory_game/memoryComponents/RegisterLogin.tsx
@@ -1,52 +1,131 @@
 import React, { useState } from "react";
+import "./RegisterLogin.css";
 
-export default function RegisterLogin(){
-const [login, setLogin] =useState("")
-const [loginPassword,setLoginPassword] = useState("")
-const [registerLogin, setRegisterLogin] = useState("")
-const [registerPassword, setRegisterPassword] = useState("")
-const [registerPasswordConfirmation, setRegisterPasswordConfirmation] = useState("")
+export default function RegisterLogin() {
+	const [login, setLogin] = useState("");
+	const [loginPassword, setLoginPassword] = useState("");
+	const [registerLogin, setRegisterLogin] = useState("");
+	const [registerPassword, setRegisterPassword] = useState("");
+	const [registerPasswordConfirmation, setRegisterPasswordConfirmation] =
+		useState("");
+	const [showLogin, setShowLogin] = useState(true);
 
-const loginOnServer = () => {
-  fetch('http://localhost:8000/login',{
-    method: 'POST',
-    headers: {'content-type':'application/json'},
-    body: JSON.stringify({'login':login,'password':loginPassword})
-  })
+	const toggleShowLogin = () => {
+		setShowLogin(!showLogin);
+	};
 
-}
+	const loginOnServer = () => {
+		fetch("http://localhost:8000/login", {
+			method: "POST",
+			headers: { "content-type": "application/json" },
+			body: JSON.stringify({ login: login, password: loginPassword }),
+		});
+	};
 
-const registerOnServer = () => {
-  fetch('http://localhost:8000/register',{
-    method: 'POST',
-    headers: {'content-type':'application/json'},
-    body: JSON.stringify({'registerLogin':registerLogin,
-    'registerPassword':registerPassword,
-    'registerPasswordConfirmation':registerPasswordConfirmation})
-  })
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error))
+	const registerOnServer = () => {
+		fetch("http://localhost:8000/register", {
+			method: "POST",
+			headers: { "content-type": "application/json" },
+			body: JSON.stringify({
+				registerLogin: registerLogin,
+				registerPassword: registerPassword,
+				registerPasswordConfirmation: registerPasswordConfirmation,
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((error) => console.error(error));
+	};
 
-}
+	const LoginForm = (
+		<div>
+			<div className="log-in-form">
+				<input
+					type="text"
+					className="typing-box"
+					name="Login"
+					placeholder="Login"
+					onChange={(e) => {
+						setLogin(e.target.value);
+					}}
+				/>
+				<input
+					type="password"
+					className="typing-box"
+					name="Password"
+					placeholder="Password"
+					onChange={(e) => {
+						setLoginPassword(e.target.value);
+					}}
+				/>
+				<button
+					className="register-login-button"
+					onClick={() => {
+						loginOnServer();
+					}}
+				>
+					Login
+				</button>
+				<button
+					className="register-login-button"
+					onClick={() => {
+						toggleShowLogin();
+					}}
+				>
+					Register Yourself!
+				</button>
+			</div>
+		</div>
+	);
 
-    const registerAndLoginForm =(
-      <div>
-        <div className="log-in-form">
-          <input type="text" className="typing-box" name="Login" onChange={(e)=>{setLogin(e.target.value)}}/>
-          <input type="password" className="typing-box" name="Password" onChange={(e)=>{setLoginPassword(e.target.value)}}/>
-          <button onClick={()=>{loginOnServer()}}>Login</button>
-        </div>
-        <div className="sign-in-form">
-          <input type="text" className="typing-box" name="RegisterLogin" onChange={(e)=>{setRegisterLogin(e.target.value)}}/>
-          <input type="text" className="typing-box" name="RegisterPassword" onChange={(e)=>{setRegisterPassword(e.target.value)}}/>
-          <input type="text" className="typing-box" name="RegisterPasswordConfirmation" onChange={(e)=>{setRegisterPasswordConfirmation(e.target.value)}}/>
-          <button onClick={()=>{registerOnServer()}}>Register</button>
-        </div>
-        </div>
-    )
-
-    return(<>
-    {registerAndLoginForm}
-    </>)
+	const RegisterForm = (
+		<div>
+			<div className="sign-in-form">
+				<input
+					type="text"
+					className="typing-box"
+					name="RegisterLogin"
+					placeholder="Login"
+					onChange={(e) => {
+						setRegisterLogin(e.target.value);
+					}}
+				/>
+				<input
+					type="text"
+					className="typing-box"
+					name="RegisterPassword"
+					placeholder="Password"
+					onChange={(e) => {
+						setRegisterPassword(e.target.value);
+					}}
+				/>
+				<input
+					type="text"
+					className="typing-box"
+					name="RegisterPasswordConfirmation"
+					placeholder="Confirm password"
+					onChange={(e) => {
+						setRegisterPasswordConfirmation(e.target.value);
+					}}
+				/>
+				<button
+					className="register-login-button"
+					onClick={() => {
+						registerOnServer();
+					}}
+				>
+					Register
+				</button>
+				<button
+					className="register-login-button"
+					onClick={() => {
+						toggleShowLogin();
+					}}
+				>
+					Back to log-in
+				</button>
+			</div>
+		</div>
+	);
+	return <>{showLogin ? LoginForm : RegisterForm}</>;
 }
--- src/config/DesignerObject.tsx
+++ src/config/DesignerObject.tsx
@@ -1,17 +1,29 @@
-
-
 interface DSOitem {
-    [name: string]: number
+	[name: string]: number;
 }
 
 interface DSOitems {
-    [key: string]: DSOitem
-
+	[key: string]: DSOitem;
 }
-export let skillList = ["css/sass", "react/typescript", "node/express","database/SQL"]
+
+export let skillList = [
+	"css/sass",
+	"react/typescript",
+	"node/express",
+	"database/SQL",
+];
 
 export const designerSkillObject: DSOitems = {
-    "Szymon B.": { "css/sass": 4, "react/typescript": 3, "node/express": 2, "database/SQL":1 }
-    ,
-    "Bogdan T.": { "css/sass": 2, "react/typescript": 2, "node/express": 2,"database/SQL":1 },
-}
+	"Szymon B.": {
+		"css/sass": 4,
+		"react/typescript": 3,
+		"node/express": 2,
+		"database/SQL": 1,
+	},
+	"Bogdan T.": {
+		"css/sass": 2,
+		"react/typescript": 2,
+		"node/express": 2,
+		"database/SQL": 1,
+	},
+};
